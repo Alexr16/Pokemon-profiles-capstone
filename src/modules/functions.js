7d6804e20aa102ec1af1f24948b8382a1351f95f
Alexr16 <j.alex-1996@hotmail.com>
2022-04-11 16:22:11 -0500

    Add new id

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,7 +1,7 @@
 import { display } from './dom.js';
 
 const createCards = () => {
-  const monsters = ['charizard', 'pikachu', 'arceus', 'magmar', 'dragonite', 'mewtwo', 'arcanine', 'lugia'];
+  const monsters = ['charizard', 'pikachu', 'arceus', 'magmar', 'dragonite', 'mewtwo', 'arcanine', 'lugia', 'altaria'];
   monsters.forEach((pokemon) => display(pokemon));
 };
 
