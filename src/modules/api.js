36516ecd5fbc3c7b03995fde520f0fd86dce6d8e
Alexr16 <j.alex-1996@hotmail.com>
2022-04-12 13:43:59 -0500

    Add involvement api functions (create, get)

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,7 +1,39 @@
+const baseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+const appId = 'AWM7JxII6f3dlrfRP916';
+const urlLikes = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/AWM7JxII6f3dlrfRP916/likes/';
+
 const getPokemon = async (id) => {
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
   const data = await response.json();
   return data;
 };
 
-export { getPokemon };
+const createApp = async () => {
+  const response = await fetch(baseURL, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+  });
+  return response.json();
+};
+
+const createLikes = async (data) => {
+  const response = await fetch(urlLikes, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify({ item_id: data.item_id }),
+  });
+  const res = await response.json();
+  return res;
+};
+
+const getLikes = async () => {
+  const response = await fetch(urlLikes);
+  const likes = await response.json();
+  return likes;
+};
+
+export { getPokemon, createLikes, getLikes };
