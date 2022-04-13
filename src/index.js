e776bd8ebde1b6880d7b032fa76977f93b2b25f1
Alexr16 <j.alex-1996@hotmail.com>
2022-04-13 17:22:48 -0500

    restore branch

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import './style.scss';
-import createCards from './modules/functions.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  createCards();
-});
--- src/modules/functions.js
+++
@@ -1,9 +0,0 @@
-import display from './dom.js';
-import { idArray } from './api.js';
-
-const monsters = idArray();
-const createCards = () => {
-  monsters.forEach((pokemon) => display(pokemon));
-};
-
-export default createCards;
